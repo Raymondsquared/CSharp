430f2dfba2fd123a987d5e60714b8ee9f8310bde
Raymond <raymond.raymond@carsales.com.au>
2016-05-04 11:45:53 +1000

    abstract vs sealed class

---
+++ CSharp.WebAPI/Controllers/ClassController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace CSharp.WebAPI.Controllers
+{
+    public class ClassController : ApiController
+    {
+        [Route("api/v1/class/abstract")]
+        public string AbstractClass()
+        {
+            return "The abstract keyword enables you to create classes and class members that are incomplete and must be implemented in a derived class.";
+        }
+        [Route("api/v1/class/sealed")]
+        public string SealedClass()
+        {
+            return "The sealed keyword enables you to prevent the inheritance of a class or certain class members that were previously marked virtual.";
+        }  
+    }
+}
